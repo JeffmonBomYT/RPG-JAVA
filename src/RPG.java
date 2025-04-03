d58b29724d12d0f7f56b953a32baf25719b18e0c
Jefferson Maschio <jeffersonluizdmaschio@gmail.com>
2025-04-03 20:35:49 -0300

    att3

--- src/RPG.java
+++ src/RPG.java
@@ -21,7 +21,7 @@ public class RPG {
      //...
      String nome;
      //BOOLEAN
-     Boolean stop = true, mons = true, mons_death = true;
+     Boolean stop = true, mons = true;
      //INIMIGOS
      int mons_hp, mons_hpmax, mons_ad, mons_xp, mons_nivel;
 //_______________________________________________________________
@@ -33,7 +33,29 @@ public class RPG {
          String opc_op = main.scan.next().toLowerCase();
          
          if (opc_op.equals("s")) {
-              
+            System.out.println("\nSEJA BEM-VINDO AO MUNDO DE THURFEND");
+            System.out.println("\nVOCE ESTÁ AMBIENTADO COM JOGOS DE TEXT-RPG? - S/N");
+            System.out.print("\n> ");
+            String opc_tutorial = main.scan.next().toUpperCase();
+    
+            switch (opc_tutorial) {
+                case "N":
+                    System.out.println("\nEm um text-rpg, voce decide acoes digitando letras ou textos no prompt quando solicitado, resultando em uma acao. ");
+                    System.out.println("___________________________________");
+                    break;
+    
+                case "S":
+                    break;
+                default:
+                    System.out.println("Comando digitado incrretamente.");
+            }
+    
+            System.out.println("\nQUAL SERÁ O NOME DO HERÓI QUE PARTICIPARÁ DE CANTIGAS DE BARDOS?");
+            System.out.print("\n> ");
+            main.nome = main.scan.next();
+            System.out.println("\n"+main.nome+"? BELO NOME, PORÉM, SÓ COM NOME NÃO SE DERROTA O MAL. ENTÃO, QUAL SERÁ SUA SEU ESTILO DE COMBATE?");
+            
+            choice_classe(main); 
          }
          else if (opc_op.equals("n")) {
               System.out.println("Adeus.");
@@ -42,70 +64,70 @@ public class RPG {
          
     }
 //_______________________________________________________________
-public static void choice_classe(RPG main) {       
-        
-    while (main.stop) { 
-        System.out.println(" ________________________");
-        System.out.println("|   ESCOLHA SUA CLASSE   |");
-        System.out.println("|------------------------|");
-        System.out.println("| - Cavaleiro            |");
-        System.out.println("| - Mago                 |");
-        System.out.println("| - Arqueiro             |");
-        System.out.println("|________________________|");
-        System.out.print("\n> ");
-        main.opc_class = main.scan.next().toUpperCase();                                           
-        
-        if (main.opc_class.equals("CAVALEIRO") || main.opc_class.equals("MAGO") || main.opc_class.equals("ARQUEIRO")) {
-            main.stop = false;
-        }
-        else {
-            System.out.println("Essa classe não existe ou foi escrita incorretamente.");
-        }
-
-        System.out.println("\nDesejas ver os Status de sua classe?");
-        System.out.println("S/N");
-        System.out.print("\n> ");
-        main.opc_status = main.scan.next().toUpperCase();
-
-        if (main.opc_status.equals("S")) {
-            if (main.opc_class.equals("MAGO")) {
-                main.mago_hp = 12;
-                main.mago_hpmax = 12;
-                main.mago_hab1 = 6;
-                main.mago_hab2 = 9;//6 * 1.5
-                main.mago_hab3 = 15;
-                System.out.println("\n[Hp: "+main.mago_hp+"/"+main.mago_hpmax+"]"+"\n[Hab1: "+main.mago_hab1+"]"+"\n[Hab2: "+main.mago_hab2+"]"+"\n[Hab3: "+main.mago_hab3+"]\n");
-            }
-            else if (main.opc_class.equals("CAVALEIRO")) {
-                main.cavaleiro_hp = 20;
-                main.cavaleiro_hpmax = 20;
-                main.cavaleiro_hab1 = 10; 
-                main.cavaleiro_hab2 = 15;//12 + 3;
-                main.cavaleiro_hab3 = 20;
-                System.out.println("\n[Hp: "+main.cavaleiro_hp+"/"+main.cavaleiro_hpmax+"]"+"\n[Hab1: "+main.cavaleiro_hab1+"]"+"\n[Hab2: "+main.cavaleiro_hab2+"]"+"\n[Hab3: "+main.cavaleiro_hab3+"]\n");              
-            }
-            else if (main.opc_class.equals("ARQUEIRO")) {
-                main.arqueiro_hp = 16;
-                main.arqueiro_hpmax = 16;
-                main.arqueiro_hab1 = 9; 
-                main.arqueiro_hab2 = 12;//9 + 3; 
-                main.arqueiro_hab3 = 18;
-                System.out.println("\n[Hp: "+main.arqueiro_hp+"/"+main.arqueiro_hpmax+"]"+"\n[Hab1: "+main.arqueiro_hab1+"]"+"\n[Hab2: "+main.arqueiro_hab2+"]"+"\n[Hab3: "+main.arqueiro_hab3+"]\n");                
-            }
-
-        }//if 
-        else if (main.opc_status.equals("N")) {
+    public static void choice_classe(RPG main) {       
             
-        }
-        else {
-            System.out.println("Comando inválido");
-            main.stop = true;
-        }
-    
+        while (main.stop) { 
+            System.out.println(" ________________________");
+            System.out.println("|   ESCOLHA SUA CLASSE   |");
+            System.out.println("|------------------------|");
+            System.out.println("| - Cavaleiro            |");
+            System.out.println("| - Mago                 |");
+            System.out.println("| - Arqueiro             |");
+            System.out.println("|________________________|");
+            System.out.print("\n> ");
+            main.opc_class = main.scan.next().toUpperCase();                                           
+            
+            if (main.opc_class.equals("CAVALEIRO") || main.opc_class.equals("MAGO") || main.opc_class.equals("ARQUEIRO")) {
+                main.stop = false;
+            }
+            else {
+                System.out.println("Essa classe não existe ou foi escrita incorretamente.");
+            }
 
-    }//while       
+            System.out.println("\nDesejas ver os Status de sua classe?");
+            System.out.println("S/N");
+            System.out.print("\n> ");
+            main.opc_status = main.scan.next().toUpperCase();
 
-} 
+            if (main.opc_status.equals("S")) {
+                if (main.opc_class.equals("MAGO")) {
+                    main.mago_hp = 12;
+                    main.mago_hpmax = 12;
+                    main.mago_hab1 = 6;
+                    main.mago_hab2 = 9;//6 * 1.5
+                    main.mago_hab3 = 15;
+                    System.out.println("\n[Hp: "+main.mago_hp+"/"+main.mago_hpmax+"]"+"\n[Hab1: "+main.mago_hab1+"]"+"\n[Hab2: "+main.mago_hab2+"]"+"\n[Hab3: "+main.mago_hab3+"]\n");
+                }
+                else if (main.opc_class.equals("CAVALEIRO")) {
+                    main.cavaleiro_hp = 20;
+                    main.cavaleiro_hpmax = 20;
+                    main.cavaleiro_hab1 = 10; 
+                    main.cavaleiro_hab2 = 15;//12 + 3;
+                    main.cavaleiro_hab3 = 20;
+                    System.out.println("\n[Hp: "+main.cavaleiro_hp+"/"+main.cavaleiro_hpmax+"]"+"\n[Hab1: "+main.cavaleiro_hab1+"]"+"\n[Hab2: "+main.cavaleiro_hab2+"]"+"\n[Hab3: "+main.cavaleiro_hab3+"]\n");              
+                }
+                else if (main.opc_class.equals("ARQUEIRO")) {
+                    main.arqueiro_hp = 16;
+                    main.arqueiro_hpmax = 16;
+                    main.arqueiro_hab1 = 9; 
+                    main.arqueiro_hab2 = 12;//9 + 3; 
+                    main.arqueiro_hab3 = 18;
+                    System.out.println("\n[Hp: "+main.arqueiro_hp+"/"+main.arqueiro_hpmax+"]"+"\n[Hab1: "+main.arqueiro_hab1+"]"+"\n[Hab2: "+main.arqueiro_hab2+"]"+"\n[Hab3: "+main.arqueiro_hab3+"]\n");                
+                }
+
+            }//if 
+            else if (main.opc_status.equals("N")) {
+                
+            }
+            else {
+                System.out.println("Comando inválido");
+                main.stop = true;
+            }
+        
+
+        }//while       
+
+    } 
 //_______________________________________________________________
     public static void mons_atq(RPG main) {
 
@@ -248,7 +270,7 @@ public static void choice_classe(RPG main) {
 //_______________________________________________________________                                                           
     public static void fight(RPG main) {       
 
-        while (main.mons_death) {
+        while (main.mons_hp > 0) {
             switch (main.opc_class) {             
                 case "MAGO":
                     System.out.println("\n|-----------------------------|");
@@ -261,15 +283,15 @@ public static void choice_classe(RPG main) {
                 
                 switch (main.opc_fight) {                   
                     case 1:
-                        main.mons_hp = main.mons_hpmax - 6;
+                        main.mons_hp -= 6;
                     break;
                     
                     case 2:
-                        main.mons_hp = main.mons_hpmax - 9;
+                        main.mons_hp -= 9;
                     break;
                     
                     case 3:
-                        main.mons_hp = main.mons_hpmax - 15;
+                        main.mons_hp -= 15;
                     break;
                     
                     case 4:
@@ -294,15 +316,15 @@ public static void choice_classe(RPG main) {
                 
                 switch (main.opc_fight) {                   
                     case 1:
-                        main.mons_hp = main.mons_hpmax - 10;
+                        main.mons_hp -= 10;
                     break;
                         
                     case 2:
-                        main.mons_hp = main.mons_hpmax - 15;
+                        main.mons_hp -= 15;
                     break;
                     
                     case 3:
-                        main.mons_hp = main.mons_hpmax - 20;
+                        main.mons_hp -= 20;
                     break;
                     
                     case 4:
@@ -326,15 +348,15 @@ public static void choice_classe(RPG main) {
                 
                 switch (main.opc_fight) {                   
                     case 1:
-                        main.mons_hp = main.mons_hpmax - 9;
+                        main.mons_hp -= 9;
                     break;
                     
                     case 2:
-                        main.mons_hp = main.mons_hpmax - 12;
+                        main.mons_hp -= 12;
                     break;
                     
                     case 3:
-                        main.mons_hp = main.mons_hpmax - 18;
+                        main.mons_hp -= 18;
                     break;
                     
                     case 4:
@@ -346,20 +368,17 @@ public static void choice_classe(RPG main) {
                 }//switch - arqueiro
         
             }//switch - geral
-                  
-            System.out.println("Voce machucou o monstro.\n"+main.mons_hp+"/"+main.mons_hpmax);
-            if (main.mons_hp > 0) {
-                main.mons_death = true;
-            }
-            else {
-                main.mons_death = false;
-            }
         
+            if (main.mons_hp <= 0) {
+                main.mons_hp = 0;
+            }
+
+            System.out.println("Voce machucou o monstro.\n"+main.mons_hp+"/"+main.mons_hpmax);
+
         }//while
 
-         if (main.mons_hp <= 0) {
               System.out.println("Parabens, voce derrotou o monstro. sua recompensa: \n["+main.mons_xp+"] Xp");
-         }
+         
         
     }
 //_______________________________________________________________
@@ -386,33 +405,13 @@ public static void choice_classe(RPG main) {
     public static void main(String[] args) {
         RPG main = new RPG();      
 
+        //op(main);
         choice_classe(main);  
         gerar_criatura(main);
         encontro(main);         
-        fight(main);
-        //op(main);
-         
-        /* System.out.println("\nSEJA BEM-VINDO AO MUNDO DE THURFEND");
-        System.out.println("\nVOCE ESTÁ AMBIENTADO COM JOGOS DE TEXT-RPG? - S/N");
-        System.out.print("\n> ");
-        String opc_tutorial = main.scan.next().toUpperCase();
-
-        switch (opc_tutorial) {
-            case "N":
-                System.out.println("\nEm um text-rpg, voce decide acoes digitando letras ou textos no prompt quando solicitado, resultando em uma acao. ");
-                System.out.println("___________________________________");
-                break;
-
-            case "S":
-                break;
-        }
-
-        System.out.println("\nQUAL SERÁ O NOME DO HERÓI QUE PARTICIPARÁ DE CANTIGAS DE BARDOS?");
-        System.out.print("\n> ");
-        main.nome = main.scan.next();
-        System.out.println("\n"+main.nome+"? BELO NOME, PORÉM, SÓ COM NOME NÃO SE DERROTA O MAL. ENTÃO, QUAL SERÁ SUA SEU ESTILO DE COMBATE?");
         
-        choice_classe(main); 
+         
+        /* 
         
         System.out.println("\nVOCE DESEJA IR PARA A CIDADE INICIAL DE <nome cidade tutorial>? - S/N");
         System.out.print("\n> ");
