1ea81a2eefb0d54df4724bd5187a805832b4ba41
Jefferson Maschio <jeffersonluizdmaschio@gmail.com>
2025-05-20 10:06:53 -0300

    swap archives

--- src/Role.java
+++ src/Role.java
@@ -1,70 +1,333 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Role {
+    String classe;
+    int hp;
+    int hpMax;
+    int dano;
+    int nvl;
+    int hab1, hab2, hab3;
+    String hab1Name, hab2Name, hab3Name;
 
-  private String name;
-  private int hp;
-  private int Hab1;
-  private int Hab2;
-  private int Hab3;
+    int mons_nivel, mons_hp, mons_hpmax, mons_xp, mons_ad;
 
-  public Role(String name, int hp, int Hab1, int Hab2, int Hab3) {
-      this.name = name;
-      this.hp = hp;
-      this.Hab1 = Hab1;
-      this.Hab2 = Hab2;
-      this.Hab3 = Hab3;
-  }
+    int opc_char;
+    String opc_status, opc_encontro;
+    boolean mons = true;
+//________________________________________________________________________________________________
+    String escolherClasse() {
+        Scanner scan = new Scanner(System.in);
 
-  public void setName(String name) {
-      this.name = name;
-  }
+        do {
+        System.out.println("\n|----------------------|");
+        System.out.println("|  Escolha sua classe  |");
+        System.out.println("| -------------------- |");
+        System.out.println("| 1 - Cavaleiro        |");
+        System.out.println("| 2 - Mago             |");
+        System.out.println("| 3 - Arqueiro         |");
+        System.out.println("|----------------------|");
+        System.out.print("\n> ");
+        opc_char = scan.nextInt();
 
-  public void setHp(int hp) {
-      this.hp = hp;
-  }
+        switch (opc_char) {
+            case 1:          
+                classe = "Cavaleiro";
+                nvl = 1;
+                hpMax = 20;
+                hp = hpMax;
+                hab1 = 10; 
+                hab1Name = "Mellee atq";
+                hab2 = 15; //(12 + 3)
+                hab2Name = "Str. attack";
+                hab3 = 20;
+                hab3Name = "Div. Smite";
 
-  public void setSkill1(int Hab1) {
-      this.Hab1 = Hab1;
-  }
+                break;
+            case 2:
+                classe = "Mago";
+                nvl = 1;
+                hpMax = 12;
+                hp = hpMax;
+                hab1 = 6;
+                hab1Name = "Mag. attack";
+                hab2 = 9; //(6 * 1.5)
+                hab2Name = "Mag. missiles";
+                hab3 = 15;
+                hab3Name = "Fire Ball";
 
-  public void setSkill2(int Hab2) {
-      this.Hab2 = Hab2;
-  }
+                break;
+            case 3:
+                classe = "Arqueiro";
+                nvl = 1;
+                hpMax = 16;
+                hp = hpMax;
+                hab1 = 9; 
+                hab1Name = "Bow Shot";
+                hab2 = 12; //(9 + 3) 
+                hab2Name = "Pres. Shot";
+                hab3 = 18;
+                hab3Name = "Giant Arrow";
 
-  public void setSkill3(int Hab3) {
-      this.Hab3 = Hab3;
-  }
+                break;
+            default:
+                System.out.println("\nClasse não existe ou foi escrita incorretamente");
+        } 
+        
+        } while (opc_char > 3 || opc_char < 1);   
+        
+            System.out.println("\nDesejas ver os status de sua classe? S/N");
+            System.out.print("\n> ");
+            opc_status = scan.next().toUpperCase();
 
-  public String getName() {
-      return name;
-  }
+            if (opc_status.equals("S")) {
+                    System.out.println("\n|-------------------|");
+                    System.out.println(" Classe: "+classe);
+                    System.out.println(" HpMax: "+hpMax);
+                    System.out.println(" Hab1: "+hab1);
+                    System.out.println(" Hab2: "+hab2);
+                    System.out.println(" Hab3: "+hab3);
+                    System.out.println("|-------------------|\n");
+            }
+       
+        return classe;
 
-  public int getHp() {
-      return hp;
-  }
+    }
+//________________________________________________________________________________________________
+    void gerar_criatura() {
+        Random rnd = new Random();
+         String[] lista_mons = {"Slime", "Esqueleto", "Orc", "Coelho assasino", "Lobo"};
+         String mons_chose = lista_mons[rnd.nextInt(lista_mons.length)];         
 
-  public int getSkill1() {
-      return Hab1;
-  }
+        switch (mons_chose) {
+            case "Slime" -> mons_nivel = rnd.nextInt(1, 2);
+            case "Esqueleto" -> mons_nivel = rnd.nextInt(2,4);
+            case "Orc" -> mons_nivel = rnd.nextInt(4, 6);
+            case "Coelho assasino" -> mons_nivel = rnd.nextInt(1, 3);
+            case "Lobo" -> mons_nivel = rnd.nextInt(2, 5);
+        } 
 
-  public int getSkill2() {
-      return Hab2;
-  }
+        switch (mons_nivel) {
+            case 1 -> {
+                mons_hpmax = rnd.nextInt(1, 7);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(1, 5);
+            }
+            case 2 -> {
+                mons_hpmax = rnd.nextInt(6, 11);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(4, 10);
+            }
+            case 3 -> {
+                mons_hpmax = rnd.nextInt(10, 15);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(9, 13);
+            }
+            case 4 -> {
+                mons_hpmax = rnd.nextInt(14, 20);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(12, 17);
+            }
+            case 5 -> {
+                mons_hpmax = rnd.nextInt(19, 25);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(16, 22);
+            }
+            case 6 -> {
+                mons_hpmax = rnd.nextInt(24, 30);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(21, 29);
+            }
+            case 7 -> {
+                mons_hpmax = rnd.nextInt(29, 36);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(28, 33);
+            }
+            case 8 -> {
+                mons_hpmax = rnd.nextInt(35, 42);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(32, 40);
+            }
+            case 9 -> {
+                mons_hpmax = rnd.nextInt(41, 50);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(39, 45);
+            }
+            case 10 -> {
+                mons_hpmax = rnd.nextInt(49, 56);
+                mons_hp = mons_hpmax;
+                mons_ad = rnd.nextInt(44, 49);
+            }
+         }
+         //Xp
+         mons_xp = mons_hpmax + mons_ad;
 
-  public int getSkill3() {
-      return Hab3;
-  }
+         System.out.println("Surge um monstro, o "+mons_chose+
+                             " [Nvl. "+mons_nivel+"]"+
+                            "\n[Vida: "+mons_hp+"/"+mons_hpmax+"]"+
+                            "\n[Dano: "+mons_ad+"]\n");
+         
+    }     
+//________________________________________________________________________________________________
+    void mons_atq() {
+        do {
+            hp -= mons_ad;
+            if (mons_hp > 0) {
+                System.out.println("O monstro te atacou");
+                System.out.println(hp+"/"+hpMax+"\n");
+            }
 
-  public int getChoice(int choice) {
-      switch (choice) {
-          case 1:
-              return Hab1;
-          case 2:
-              return Hab2;
-          case 3:
-              return Hab3;
-          default:
-              break;
-      }
-      return 0;
-  }
+            if (hp <= 0 && mons_hp > 0) {
+                hp = 0;
+                System.out.println("Voce foi derrotado. Eu tinha grandes esperanças em você, que decepção!");
+                break;
+            }
+            else {
+                System.out.println("Parabéns, você derrotou o monstro. sua recompensa: \n["+mons_xp+"] Xp");
+            }
+
+            return;
+
+        } while (mons_hp >= 0);
+
+    }
+//________________________________________________________________________________________________
+    void encontro() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("[Hp: "+hp+"/"+hpMax+"]"+
+        "\n[Hab1: "+hab1+"]"+
+        "\n[Hab2: "+hab2+"]"+
+        "\n[Hab3: "+hab3+"]\n");
+
+         while (mons) {
+         System.out.println("|----------------|");
+         System.out.println("|  Fight     Bag |");
+         System.out.println("|                |");
+         System.out.println("|  Swap      Run |");
+         System.out.println("|----------------|");
+         System.out.print("\n> ");
+         opc_encontro = scan.next().toLowerCase();
+              
+              switch (opc_encontro) {
+                 case "fight":
+                    //...
+                    mons = false;
+                   break;
+                 case "bag":
+                    //...
+                    mons = false;
+                 
+                   break;
+                 case "swap":
+                    //...
+                       mons = false;
+                       break;                   
+                 case "run":
+                    //...
+                    System.out.println("Sistema não implementado");
+                    mons = false;
+                   break;
+                 default:
+                    System.out.println("Comando inválido. tente novamente");                   
+              }//switch
+              
+         }
+    }
+//________________________________________________________________________________________________
+    void fight() {       
+
+        System.out.println("\n|--------------------------------------|");
+        System.out.format(" 1 - %s ", hab1Name);
+        System.out.format("| %s - 2", hab2Name);
+        System.out.println("\n ");
+        System.out.format(" 3 - %s ", hab3Name);
+        System.out.format("| Back - 4");
+        System.out.println("\n|--------------------------------------|");
+    
+    }
+//________________________________________________________________________________________________
+
+//________________________________________________________________________________________________
+
+
+
+
 }
+
+
+
+
+        //case "MAGO":
+        //          Classe = new int[] {/*Hp*/12,/*HPMax*/12, /*Hab1*/6, /*Hab2*/9, /*Hab3*/15};
+
+/*
+
+ IDEIAS:
+
+- tela inicial ##
+    L> escolher nome ##
+- escolher classe 
+
+
+- ## gerar um monstro  
+- ## encontro com monstros  
+     L> talvez batalha dupla
+    ## L> interface de batalha 
+    ## L> interface de espólio / derrota
+
+- cidades
+- viajem
+    L> chance de algo acontecer
+
+- descanso    
+    L> estabelecimentos
+    L> ao ar livre
+
+
+---------------------------------|
+<cidade inicial>
+- acampamento de guerra
+- general - guia de introducao 
+                 L> 1º combate
+                 L> acoes basicas
+
+---------------------------------|
+TIRAR TEMPORARIAMENTE:
+- por ora, tirar melle de "arqueiro"
+- escolher 1 aliado 
+
+________________________________________
+
+INTERFACE DE ENCONTRO
+System.out.println("|----------------|");
+System.out.println("|  Fight     Bag |");
+System.out.println("|                |");
+System.out.println("|  Swap      Run |");
+System.out.println("|----------------|");
+
+INTERCADE DE LUTA
+System.out.println("|----------------|");
+System.out.println("|  Hab1     Hab2 |");
+System.out.println("|                |");
+System.out.println("|  Hab3     Back |");
+System.out.println("|----------------|");
+
+//MAGO
+mago_hpmax = 12;
+mago_hab1 = 6;
+mago_hab2 = 9; (6 * 1.5)
+mago_hab3 = 15;
+         
+//CAVALEIRO          
+cavaleiro_hpmax = 20;
+cavaleiro_hab1 = 10; 
+cavaleiro_hab2 = 15; (12 + 3)
+cavaleiro_hab3 = 20;
+                 
+//ARQUEIRO
+arqueiro_hpmax = 16;
+arqueiro_hab1 = 9; 
+arqueiro_hab2 = 12; (9 + 3) 
+arqueiro_hab3 = 18;
+ */
---
+++ src/testPlay.java
@@ -0,0 +1,11 @@
+public class testPlay {
+    public static void main(String[] args) {
+        Role role = new Role();
+
+        role.escolherClasse();
+        /* role.gerar_criatura();
+        role.mons_atq();
+        role.encontro(); */
+        role.fight();
+    }
+}
